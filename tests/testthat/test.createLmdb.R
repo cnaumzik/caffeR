8f265dfd596d263bbcbd874616e52cd8835d6f15
sfeuerriegel <stefan.feuerriegel@is.uni-freiburg.de>
2016-07-18 15:54:23 +0200

    Fixed test

--- tests/testthat/test.createLmdb.R
+++ tests/testthat/test.createLmdb.R
@@ -5,6 +5,7 @@ test_that("create script performs correct replacements", {
   script <- generateCreateLmdb(caffedir = "~/Documents/caffe", name = "MyModel",
                                caffe_preprocessing = TRUE,
                                resize_height = 100, resize_width = 100)
+  script <- paste(script, collapse="\n")
 
-  expect_match(script, "~/Documents/caffe/examples/MyModel")
+  expect_match(script, "EXAMPLE=~/Documents/caffe/examples/MyModel")
 })
