f95513346f9c10b9de66ae789183566cc5f92f07
Christof Naumzik <christof.naumzik@is.uni-freiburg.de>
2016-07-20 11:32:09 +0200

    *Adapted make_imagenet.sh file and writeMakeMean similar to createLmdb.R

--- R/writemakemean.R
+++ R/writemakemean.R
@@ -1,47 +1,22 @@
 #' @export
-writeMakeMean <- function(name = "MyModel", caffedir = "~/Documents/caffe") {
-    #This function assumes that you have downloaded the imagent example from TODO and that the function
-    #make_imagenet_mean.sh is stored in /path/to/caffe/examples/imagenet
-    #Do not set resize parameter if you use TODO to preprocess the images
+writeMakeMean <- function(name = "MyModel",caffedir = "~/Documents/caffe"){
+  #This function assumes that you have downloaded the imagent example from TODO and that the function
+  #create_imagenet.sh is stored in /path/to/caffe/examples/imagenet
+  #Do not set resize parameter if you use TODO to preprocess the images
 
-    on.exit(closeAllConnections())
+  on.exit(closeAllConnections())
 
-    source_path <-
-      paste0(caffedir, "/examples/imagenet/make_imagenet_mean.sh")
+  script <- generateMakeMean(caffedir, name)
 
-    target_path <- paste0(caffedir, "/models/", name, "/make_mean.sh")
+  output_path <- paste0(caffedir, "/models/", name, "/make_mean.sh")
 
+  writeLines(script, con = output_path)
+}
 
-    fileMakeMean <- readLines(source_path)
+generateMakeMean <- function(caffedir, name) {
 
-
-    fileMakeMean[5] <- paste0("EXAMPLE=", caffedir, "/examples/", name)
-
-
-    fileMakeMean[6] <- paste0("DATA=", caffedir, "/data/", name)
-
-
-    fileMakeMean[7] <- paste0("TOOLS=", caffedir, "/build/tools")
-
-
-    fileMakeMean[9] <-
-      paste0("$TOOLS/compute_image_mean -backend=lmdb $EXAMPLE/",
-             name,
-             "_train_lmdb \\")
-
-
-    fileMakeMean[10] <- paste0("$DATA/", name, "_train_mean.binaryproto")
-
-
-    fileMakeMean[12] <-
-      paste0("$TOOLS/compute_image_mean -backend=lmdb $EXAMPLE/", name, "_val_lmdb \\")
-
-
-    fileMakeMean[13] <- paste0("$DATA/", name, "_val_mean.binaryproto")
-
-
-
-    write(fileMakeMean, target_path, append = FALSE)
-    write("", target_path, append = TRUE)
-    write("echo \"Done.\" ", target_path, append = TRUE)
-  }
+  script <- readLines(system.file("extdata", "make_imagenet_mean.sh", package = "caffeR"))
+  script <- gsub("__CAFFEDIR__", caffedir, script)
+  script <- gsub("__NAME__", name, script)
+  return(script)
+}
--- inst/extdata/make_imagenet_mean.sh
+++ inst/extdata/make_imagenet_mean.sh
@@ -2,11 +2,14 @@
 # Compute the mean image from the imagenet training lmdb
 # N.B. this is available in data/ilsvrc12
 
-EXAMPLE=examples/imagenet
-DATA=data/ilsvrc12
-TOOLS=build/tools
+EXAMPLE=__CAFFEDIR__/examples/__NAME__
+DATA=__CAFFEDIR__/data/__NAME__
+TOOLS=__CAFFEDIR__/build/tools
 
-$TOOLS/compute_image_mean $EXAMPLE/ilsvrc12_train_lmdb \
-  $DATA/imagenet_mean.binaryproto
+$TOOLS/compute_image_mean -backend=lmdb $EXAMPLE/__NAME___train_lmdb \
+  $DATA/__NAME___train_mean.binaryproto
+
+$TOOLS/compute_image_mean -backend=lmdb $EXAMPLE/__NAME___val_lmdb \
+  $DATA/__NAME___val_mean.binaryproto
 
 echo "Done."
