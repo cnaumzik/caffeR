fa0a3231c41af081bd1c61b10f38b03323dd9985
Christof Naumzik <christof.naumzik@is.uni-freiburg.de>
2016-07-21 11:08:59 +0200

    *Fixed small typo

--- R/writemakemean.R
+++ R/writemakemean.R
@@ -1,5 +1,5 @@
 #' @export
-writeMakeMean <- function(name = "MyModel",caffedir = "~/Documents/caffe"){
+writeMakeMean <- function(caffedir = "~/Documents/caffe" , name = "MyModel"){
   #This function assumes that you have downloaded the imagent example from TODO and that the function
   #create_imagenet.sh is stored in /path/to/caffe/examples/imagenet
   #Do not set resize parameter if you use TODO to preprocess the images
