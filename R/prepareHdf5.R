267ac373e7fd3fe54c2c65e421953fa134ff0305
Christof Naumzik <christof.naumzik@is.uni-freiburg.de>
2016-07-26 20:08:18 +0200

    *Fixed Description and NAMESPACE *Adapted prepareHdf5.R to fit in local folder structure

--- R/prepareHdf5.R
+++ R/prepareHdf5.R
@@ -1,3 +1,4 @@
+#'@export
 prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
                         name = "MyModel" ,
                         imagedir = "~/main" ,
@@ -16,11 +17,11 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
   if(is.null(labels)){
     stop ("The labels are required.")
   }
-  file_name <- "~/Promotion/train.h5"
+  file_name <- paste0(caffedir,"/data/",name,"/",phase,".h5")
   on.exit(closeAllConnections())
   
   
-  h5createFile(file_name)
+  rhdf5::h5createFile(file_name)
   n <- length(values)
   
   if(n != length(image_ids)){
@@ -28,13 +29,13 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
   }
   data_batch <- array(0, dim = c(batch_size,3,resize_width,resize_height))
   label_batch <- array(0, dim = c(batch_size,1))
-  h5createDataset(file_name , "data", c(n,3,resize_width,resize_height), storage.mode = "double", showWarnings = FALSE,level=9 ,chunk = c(batch_size,3,resize_width,resize_height))
-  h5createDataset(file_name , "label", c(n,1), storage.mode = "double", showWarnings = FALSE,level = 9, chunk =c(batch_size,1))
+  rhdf5::h5createDataset(file_name , "data", c(n,3,resize_width,resize_height), storage.mode = "double", showWarnings = FALSE,level=9 ,chunk = c(batch_size,3,resize_width,resize_height))
+  rhdf5::h5createDataset(file_name , "label", c(n,1), storage.mode = "double", showWarnings = FALSE,level = 9, chunk =c(batch_size,1))
   i<-1
   chunks <-0
   for(k in 1:n){
     print(k)
-    image_path <- paste0("~/Promotion/Research/Code/Data/Catsvsdogs/main/",image_ids[k],".jpg")
+    image_path <- paste0(imagedir,"/",image_ids[k],".jpg")
     print(image_path)
     data_batch[i,,,]<- preprocessImagesHdf5(image_path, padding , resize_height , resize_width)
     label_batch[i,] <- values[k]
@@ -46,13 +47,13 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
       upper<-lower+batch_size-1
       
       #h5write(data_batch, file = file_name, name ="data", index = list(lower:upper,1:3,1:resize_width,1:resize_height))
-      h5write(data_batch, file = file_name, name ="data", start = c(lower,1,1,1),count=c(batch_size,3,resize_width,resize_height))
+      rhdf5::h5write(data_batch, file = file_name, name ="data", start = c(lower,1,1,1),count=c(batch_size,3,resize_width,resize_height))
       #h5write(label_batch, file = file_name, name ="label", index = list(lower:upper,1))
-      h5write(label_batch, file = file_name, name ="label", start = c(lower,1),count=c(batch_size,1))
+      rhdf5::h5write(label_batch, file = file_name, name ="label", start = c(lower,1),count=c(batch_size,1))
       chunks <- chunks +1
     }
   }
   
-  H5close()
+  rhdf5::H5close()
   
 }
