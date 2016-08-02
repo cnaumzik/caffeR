15872ae7f68dab1dec11ce02a94c3e1b66a2dc68
Christof Naumzik <christof.naumzik@is.uni-freiburg.de>
2016-08-02 20:26:42 +0200

    *Fixed bugs

--- R/prepareHdf5.R
+++ R/prepareHdf5.R
@@ -25,11 +25,11 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
     )
   }
   n <- length(values)
-  batch_size <- adjustBatchSize(n,batch_size)
+  batch_size <- adjustBatchSize(n, batch_size)
   #Currently the is a limit on the max number of units in caffe -  splitting is required
-  INT_MAX<- 2*1024^3-1
-  max_entries <- INT_MAX/(3*resize_height*resize_width)
-  max_batches <- max(max_entries%/%batch_size,1)
+  INT_MAX <- 2 * 1024 ^ 3 - 1
+  max_entries <- floor(INT_MAX / (3 * resize_height * resize_width))
+  max_batches <- max(max_entries %/% batch_size, 1)
   
   #Image mean einlesen
   image_mean <- rhdf5::h5read(mean_file, "mean")
@@ -45,20 +45,21 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
     stop("The image mean dimensions are not correct")
   }
   
-  if(n>max_entries) {
-    num_files <-1
-    file_name <- paste0(caffedir, "/data/", name, "/", phase, ".h5_0",num_files)
+  if (n > max_entries) {
+    num_files <- 1
+    file_name <-
+      paste0(caffedir, "/data/", name, "/", phase, "_0", num_files, ".h5")
   } else {
     file_name <- paste0(caffedir, "/data/", name, "/", phase, ".h5")
   }
-  file_name <- paste0("~/Promotion/Research/Code/Data/Craigslist/train.h5")
-  createHDF5(file_name , resize_height , resize_width , max_batches*batch_size,batch_size)
-  #write(file_name,
-        #paste0(caffedir, "/data/", name, "/", phase, ".txt"),
-        #append = FALSE)
+  createHDF5(file_name ,
+             resize_height ,
+             resize_width ,
+             max_batches * batch_size,
+             batch_size)
   write(file_name,
-  paste0("~/Promotion/Research/Code/Data/Craigslist/test.txt"),
-  append = FALSE)
+        paste0(caffedir, "/data/", name, "/", phase, ".txt"),
+        append = FALSE)
   if (n != length(image_ids)) {
     stop("Number of labels and Images do not match")
   }
@@ -84,8 +85,8 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
   i <- 1
   batch_counter <- 0
   print(paste0("Processing a total of ", n, " images."))
+  last_indx <- 1
   for (k in 1:n) {
-    print(paste("Image number:", k))
     image_path <- paste0(imagedir, "/", image_ids[k], suffix, ".jpg")
     image_batch[, , , i] <-
       preprocessImagesHdf5(image_path,
@@ -98,6 +99,7 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
     
     
     if (i == batch_size) {
+      print(paste0("Saving ", last_indx%/%batch_size+1, ". batch"))
       rhdf5::h5write(
         image_batch,
         file = file_name,
@@ -106,25 +108,41 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
           1:resize_width,
           1:resize_height,
           1:3,
-          (k - batch_size + 1):k
+          last_indx:(last_indx + batch_size - 1)
         )
       )
       rhdf5::h5write(
         label_batch,
         file = file_name,
         name = "label",
-        index = list(1:1, 1:1, 1:1, (k - batch_size + 1):k)
+        index = list(1:1, 1:1, 1:1, 
+                     last_indx:(last_indx + batch_size -1)
+                     )
       )
+      rhdf5::H5close()
       i <- 0
-      batch_counter <- batch_counter + 1 
+      last_indx <- last_indx + batch_size
     }
-    if (batch_counter == max_batches && k!=n) {
+    if (last_indx == max_batches*batch_size+1 && k != n) {
       num_files <- num_files + 1
-      file_name <- paste0(caffedir, "/data/", name, "/", phase, ".h5_0",num_files)
-      generateHDF5(file_name , resize_height , resize_width , max_batches*batch_size)
+      file_name <-
+        paste0(caffedir,
+               "/data/",
+               name,
+               "/",
+               phase,
+               "_0",
+               num_files,
+               ".h5")
+      createHDF5(file_name ,
+                 resize_height ,
+                 resize_width ,
+                 max_batches * batch_size,
+                 batch_size)
       write(file_name,
             paste0(caffedir, "/data/", name, "/", phase, ".txt"),
             append = TRUE)
+      last_indx <- 1
     }
     
     i <- i + 1
@@ -138,35 +156,40 @@ prepareHdf5 <- function(caffedir = "~/Documents/caffe" ,
   
 }
 #====================================================================================================================================================================
-adjustBatchSize<-function(n,batch_size){
- if(n%%batch_size != 0){
-   adjustBatchSize(n,batch_size-1)
- } else{
-   return(batch_size)
- }
-   
+adjustBatchSize <- function(n, batch_size) {
+  if (n %% batch_size != 0) {
+    adjustBatchSize(n, batch_size - 1)
+  } else{
+    return(batch_size)
+  }
+  
 }
 #=============================================================================================================================
-createHDF5 <- function(file_name, resize_height , resize_width , n , batch_size){
-  rhdf5::h5createFile(file_name)
-  #HDF5 file needs to be in WxHxCxN since C interprets the dimensions differently than R
-  rhdf5::h5createDataset(
-    file_name ,
-    "data",
-    c(resize_width, resize_height, 3, n),
-    storage.mode = "double",
-    showWarnings = FALSE,
-    chunk = c(resize_width, resize_height, 3, batch_size),
-    level = 3
-  )
-  
-  rhdf5::h5createDataset(
-    file_name ,
-    "label",
-    c(1, 1, 1, n),
-    chunk = c(1, 1, 1, batch_size),
-    storage.mode = "double",
-    showWarnings = FALSE,
-    level = 9
-  )
-}
+createHDF5 <-
+  function(file_name,
+           resize_height ,
+           resize_width ,
+           n ,
+           batch_size) {
+    rhdf5::h5createFile(file_name)
+    #HDF5 file needs to be in WxHxCxN since C interprets the dimensions differently than R
+    rhdf5::h5createDataset(
+      file_name ,
+      "data",
+      c(resize_width, resize_height, 3, n),
+      storage.mode = "double",
+      showWarnings = FALSE,
+      chunk = c(resize_width, resize_height, 3, batch_size),
+      level = 9
+    )
+    
+    rhdf5::h5createDataset(
+      file_name ,
+      "label",
+      c(1, 1, 1, n),
+      chunk = c(1, 1, 1, batch_size),
+      storage.mode = "double",
+      showWarnings = FALSE,
+      level = 9
+    )
+  }
