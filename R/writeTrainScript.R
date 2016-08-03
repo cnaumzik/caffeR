d05c5c13c6acb85fffc46e401b4290f0fa826f0c
Christof Naumzik <christof.naumzik@is.uni-freiburg.de>
2016-08-03 18:03:58 +0200

    *Included case of not finetuning

--- R/writeTrainScript.R
+++ R/writeTrainScript.R
@@ -1,21 +1,52 @@
 #'@export
-writeTrainScript <- function (caffedir = "~/Documents/caffe" , name = "My_Model" , network_name = "bvlc_reference_caffenet"){
+writeTrainScript <-
+  function (caffedir = "~/Documents/caffe" ,
+            name = "My_Model" ,
+            network_name = NULL ,
+            finetune = FALSE) {
+    on.exit(closeAllConnections())
+    if (!finetune &&
+        !file.exists(paste0(
+          caffedir,
+          "/models/",
+          network_name,
+          "/",
+          network_name,
+          ".caffemodel"
+        ))) {
+      stop(
+        paste0(
+          "There does not exist a .caffemodel file for the given network: ",
+          network_name,
+          ". Please ensure that you have provided the correct name and dowload the .caffemodel file"
+        )
+      )
+    }
+    script <-
+      generateTrainScript(caffedir, name , network_name, finetune)
+    
+    output_path <-
+      paste0(caffedir, "/models/", name, "/train_network.sh")
+    
+    writeLines(script, con = output_path)
+    
+  }
 
-  on.exit(closeAllConnections())
-
-  script <- generateTrainScript(caffedir, name , network_name)
-
-  output_path <- paste0(caffedir, "/models/", name, "/train_network.sh")
-
-  writeLines(script, con = output_path)
-
-}
-
-generateTrainScript <- function(caffedir = "~/Documents/caffe" , name = "My_Model" , network_name = "bvlc_reference_caffenet"){
-  script <- readLines(system.file("extdata", "train_network.sh", package = "caffeR"))
-  script <- gsub("__CAFFEDIR__", caffedir, script)
-  script <- gsub("__NAME__", name, script)
-  script <- gsub("__NETWORK__", network_name, script)
-  return(script)
-
+generateTrainScript <-
+  function(caffedir = "~/Documents/caffe" ,
+           name = "My_Model" ,
+           network_name = "bvlc_reference_caffenet" ,
+           finetune = TRUE) {
+    script <-
+      readLines(system.file("extdata", "train_network.sh", package = "caffeR"))
+    script <- gsub("__CAFFEDIR__", caffedir, script)
+    script <- gsub("__NAME__", name, script)
+    if (finetune) {
+      script <- gsub("__PH__", "", script)
+      script <- gsub("__NETWORK__", network_name, script)
+    } else {
+      script <- gsub("__PH__", "#", script)
+    }
+    return(script)
+    
   }
--- inst/extdata/train_network.sh
+++ inst/extdata/train_network.sh
@@ -3,5 +3,5 @@ TOOLS=__CAFFEDIR__/build/tools/caffe
 MODEL=__CAFFEDIR__/models
 sudo $TOOLS train \
 --solver $MODEL/__NAME__/solver.prototxt \
---weights $MODEL/__NETWORK__/__NETWORK__.caffemodel 2>&1 \
+__PH__--weights $MODEL/__NETWORK__/__NETWORK__.caffemodel 2>&1 \
 | tee $MODEL/__NAME__/model.log
